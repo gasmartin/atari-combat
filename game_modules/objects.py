8bb5e6ad09f6b4e891b76afd60aa7478910a33c7
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-10-10 17:54:26 -0400

    Adicionando hitbox e giro

--- game_modules/objects.py
+++ game_modules/objects.py
@@ -7,3 +7,13 @@ def create_screen(title, width, height):
     screen.setup(width=width, height=height)
     screen.tracer(0)
     return screen
+
+def create_hitbox(x, y, color):
+    hitbox = turtle.Turtle()
+    hitbox.speed(0)
+    hitbox.color(color)
+    hitbox.shape("square")
+    hitbox.shapesize(stretch_wid=3, stretch_len=4)
+    hitbox.penup()
+    hitbox.goto(x, y)
+    return hitbox
--- main.py
+++ main.py
@@ -8,9 +8,41 @@ def close_screen():
     global playing
     playing = False
 
-screen = objects.create_screen("Atari Combat", 1000, 800)
+screen = objects.create_screen("Atari Combat", 1300, 900)
 root = screen.getcanvas().winfo_toplevel()
 root.protocol("WM_DELETE_WINDOW", close_screen)
+
+communist_hitbox = objects.create_hitbox(-520, -100, "red")
+capitalist_hitbox = objects.create_hitbox(520, -100, "blue")
+
+communist_angle = 0
+capitalist_angle = 0
+
+def communist_turn_left():
+    global communist_angle
+    communist_angle += 30
+    communist_hitbox.left(communist_angle)
+
+def communist_turn_right():
+    global communist_angle
+    communist_angle -= 30
+    communist_hitbox.right(communist_angle)
+
+def capitalist_turn_left():
+    global capitalist_angle
+    capitalist_angle += 30
+    capitalist_hitbox.left(capitalist_angle)
+
+def capitalist_turn_right():
+    global capitalist_angle
+    capitalist_angle -= 30
+    capitalist_hitbox.right(capitalist_angle)
+
+screen.listen()
+screen.onkeypress(communist_turn_left, "a")
+screen.onkeypress(communist_turn_right, "d")
+screen.onkeypress(capitalist_turn_left, "Left")
+screen.onkeypress(capitalist_turn_right, "Right")
 
 while playing:
     screen.update()
